052dd898079516c30ee8413a4505faba8fdb0b6d
igor_pont <pontigor11@gmail.com>
2022-07-29 00:39:49 +0300

    fix bag

--- roman_numerals_helper.py
+++ roman_numerals_helper.py
@@ -1,7 +1,7 @@
 # Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
 
 
-integers = dict(I=1, IV=4, V=5, X=10, L=50, C=100, D=500, M=1000)
+integers = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
 
 
 def to_roman(arabic):
@@ -36,4 +36,5 @@ def from_roman(roman):
     return str(result)
 
 
-print(to_roman('12'))
+print(to_roman('1000'))
+print(from_roman('M'))
