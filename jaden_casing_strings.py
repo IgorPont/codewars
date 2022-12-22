60ed92d9f6353a7eb8771fd8c85989f1b7e5e5c6
igor_pont <pontigor11@gmail.com>
2022-12-22 23:21:09 +0300

    add solution option jaden_casing_strings.py

--- jaden_casing_strings.py
+++ jaden_casing_strings.py
@@ -3,5 +3,10 @@ def to_jaden_case(string):
 
     return res
 
+# def to_jaden_case(string):
+#     list = string.split()
+#     new_list = [i.capitalize() for i in list]
+#     return ' '.join(new_list)
+
 
 print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
